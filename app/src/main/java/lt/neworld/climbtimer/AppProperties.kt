6f72a9a6c8bcfae44fb7f18fc567ea8653dd8235
Andrius Semionovas <andrius.semionovas@vinted.com>
2017-05-05 21:31:49 +0300

    Introduce app properties

---
+++ app/src/main/java/lt/neworld/climbtimer/AppProperties.kt
@@ -0,0 +1,12 @@
+package lt.neworld.climbtimer
+
+/**
+ * @author Andrius Semionovas
+ * @since 2017-05-05
+ */
+object AppProperties {
+    var runTime: Int = 5 * 60 * 1000
+    var waitTime: Int = 30 * 1000
+    var closeTheEnd: Int = 5 * 1000
+    var title: String = "Climb timer"
+}
